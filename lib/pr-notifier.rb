39eb907975f9598dcf31f70f30af88269f45d2c8
hatappi <hatappi@hatappi.me>
2017-12-21 21:30:52 +0900

    add pr-notifier module

--- lib/pr-notifier.rb
+++ lib/pr-notifier.rb
@@ -1,5 +1,38 @@
 require "pr-notifier/version"
+require "pr-notifier/github"
+require "erb"
+require "slack-notifier"
 
 module PrNotifier
-  # Your code goes here...
+  module_function
+  def run(gh_token: nil, repos: nil, webhook_url: nil, channel: nil, username: nil, template: nil)
+    gh_token ||= ENV['GH_TOKEN']
+    repos = (repos || ENV['GH_REPOS'])&.split(',')
+    webhook_url ||= ENV["SLACK_WEBHOOK_URL"]
+    channel ||= ENV["SLACK_CHANNEL"]
+    username ||= ENV["SLACK_USERNAME"]
+    @template = template || ENV["TEMPLATE"]
+
+    gh_client = Github.new(access_token: gh_token)
+    prs = repos.each_with_object([]) { |repo, prs| prs.concat(gh_client.fetch_prs_by(repo)) }
+
+    notifier = Slack::Notifier.new(webhook_url, channel: channel, username: username)
+    notifier.ping(message(pull_requests: prs))
+  end
+
+  def message(pull_requests:) 
+    ERB.new(template, nil, '-').result(binding)
+  end
+
+  def template
+    @template || <<~ERB
+<% pull_requests.each do |pr| -%>
+:memo: <%= pr.title %>
+      <%= pr.url %>
+<% if pr.reviewers.size > 0 -%>
+      reviewers: <%= pr.reviewers.join(", ") %>
+<% end -%>
+<% end -%>
+ERB
+  end
 end
